def283956e5d37b3108ab92f345bfea6bd21091d
JHertz5 <jukka.hertzog@live.co.uk>
2017-01-31 17:34:51 +0000

    Added function to measure temp and humi, improved general functionality

--- src/main.py
+++ src/main.py
@@ -1,22 +1,31 @@
 from machine import Pin, I2C
-import time, measure_Si7021
+import time # for sleep_ms
+import measure_Si7021 # for measurement functions
 
-i2c = I2C(scl=Pin(5),sda=Pin(4),freq=100000)
+print("\n*************************************************************")
+print("Welcome to the Sexual Keiling Temperature and Humidity Sensor\n")
+input("Press Enter to continue")
 
+i2c = I2C(scl=Pin(5),sda=Pin(4),freq=100000) #construct and initialise I2C object
+
+# read slave address of I2C sensors
 i2c_AddrList = i2c.scan()
 addr_Si7021 = i2c_AddrList[0]
 
-print("I2C address =",hex(addr_Si7021))
+print("\nI2C address =", hex(addr_Si7021), "\n")
 if addr_Si7021 != 0x40:
-    print("Address should be 0x40, but is",hex(addr_Si7021),"instead")
+    print("Address should be 0x40, but is", hex(addr_Si7021), "instead")
     quit()
 
 while 1:
 
-    temp_C = measure_Si7021.measure_temp(i2c, addr_Si7021)
-    print("Temp measured as",temp_C,"degrees C")
+#    temp_C = measure_Si7021.measure_temp(i2c, addr_Si7021)
+#    print("Temp measured as", temp_C, "degrees C")
 
-    humidity = measure_Si7021.measure_humi(i2c, addr_Si7021)
-    print("Relative Humidity measured as",humidity,"%")
+#    humidity = measure_Si7021.measure_humi(i2c, addr_Si7021)
+#    print("Relative Humidity measured as", humidity, "%")
+
+    result = measure_Si7021.measure_both(i2c, addr_Si7021)
+    print("RH measured as", result['humi'], "%, \t temp measured as", result['temp'], "oC")
 
     time.sleep_ms(250)
--- src/measure_Si7021.py
+++ src/measure_Si7021.py
@@ -1,23 +1,63 @@
+# reads temperature
+# inputs:
+#   i2c - I2C object
+#   addr_Si7021 - slave address of sensor (int)
+# outputs
+#   temperature in degrees C
 def measure_temp(i2c, addr_Si7021=0x40):
     import time
 
     temp_readCommand = bytearray([0xF3])
-    temp_minDelay = 7 #ms
+    temp_minDelay = 7 #ms, comes from datasheet
 
-    i2c.writeto(addr_Si7021, temp_readCommand)
-    time.sleep_ms(temp_minDelay)
-    temp_code = i2c.readfrom(0x40, 2)
+    i2c.writeto(addr_Si7021, temp_readCommand) # send read command
+    time.sleep_ms(temp_minDelay) # wait for conversion to be complete
+    temp_code = i2c.readfrom(0x40, 2) # read result
 
     return (175.72 * (256*temp_code[0] + temp_code[1])/65536) - 46.85
 
+# reads humidity
+# inputs:
+#   i2c - I2C object
+#   addr_Si7021 - slave address of sensor (int)
+# outputs
+#   humidity %
 def measure_humi(i2c, addr_Si7021=0x40):
     import time
 
     humi_readCommand = bytearray([0xF5])
-    humi_minDelay = 17 #ms
+    humi_minDelay = 17 #ms, comes from datasheet
 
-    i2c.writeto(addr_Si7021, humi_readCommand)
-    time.sleep_ms(humi_minDelay)
-    humi_code = i2c.readfrom(addr_Si7021, 2)
+    i2c.writeto(addr_Si7021, humi_readCommand) # send read command
+    time.sleep_ms(humi_minDelay) # wait for conversion to be complete
+    humi_code = i2c.readfrom(addr_Si7021, 2) # read result
 
     return (125*humi_code[0] + humi_code[1])/65536 - 6
+
+# reads temperature and humidity
+# inputs:
+#   i2c - I2C object
+#   addr_Si7021 - slave address of sensor (int)
+# outputs
+#   'temp' - temperature in degrees C
+#   'humi' - humidity %
+def measure_both(i2c, addr_Si7021=0x40):
+    import time
+
+    humi_readCommand = bytearray([0xF5])
+    humi_minDelay = 17 #ms, comes from datasheet
+    # this command reads temp from previous humi measurement
+    temp_readCommand_noMeasure = bytearray([0xE0])
+
+    # measure and read humidity
+    i2c.writeto(addr_Si7021, humi_readCommand) # send read command
+    time.sleep_ms(humi_minDelay) # wait for conversion to be complete
+    humi_code = i2c.readfrom(addr_Si7021, 2) # read result
+    # read temp
+    i2c.writeto(addr_Si7021, temp_readCommand_noMeasure) # send read command
+    temp_code = i2c.readfrom(addr_Si7021, 2) # read result
+
+    humi = (125*humi_code[0] + humi_code[1])/65536 - 6
+    temp = (175.72 * (256*temp_code[0] + temp_code[1])/65536) - 46.85
+
+    return { 'temp':temp, 'humi':humi }
