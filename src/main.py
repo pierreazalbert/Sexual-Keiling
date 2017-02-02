4956386e884c445b65821f12ec115b2617e38632
JHertz5 <jukka.hertzog@live.co.uk>
2017-02-02 10:04:29 +0000

    Standardised string markers as ' rather than "

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from machine import Pin, I2C
 import time # for sleep_ms
 import measure_Si7021 # for measurement functions
+from umqtt.simple import MQTTClient
 
-print("\n*************************************************************")
-print("Welcome to the Sexual Keiling Temperature and Humidity Sensor\n")
-input("Press Enter to continue\n")
+
+print('\n*************************************************************')
+print('Welcome to the Sexual Keiling Temperature and Humidity Sensor\n')
+input('Press Enter to continue\n')
 
 i2c = I2C(scl=Pin(5),sda=Pin(4),freq=100000) #construct and initialise I2C object
 
@@ -12,20 +14,20 @@ i2c = I2C(scl=Pin(5),sda=Pin(4),freq=100000) #construct and initialise I2C objec
 i2c_addr_list = i2c.scan()
 addr_Si7021 = i2c_addr_list[0]
 
-print("I2C address =", hex(addr_Si7021), "\n")
+print('I2C address =', hex(addr_Si7021), '\n')
 if addr_Si7021 != 0x40:
-    print("FAILURE: Address of Si7021 should be 0x40, but is", hex(addr_Si7021), "instead\n")
+    print('FAILURE: Address of Si7021 should be 0x40, but is', hex(addr_Si7021), 'instead\n')
     quit()
 
 while 1:
 
 #    temp_C = measure_Si7021.measure_temp(i2c, addr_Si7021)
-#    print("Temp measured as", temp_C, "degrees C")
+#    print('Temp measured as', temp_C, 'degrees C')
 
 #    humidity = measure_Si7021.measure_humi(i2c, addr_Si7021)
-#    print("Relative Humidity measured as", humidity, "%")
+#    print('Relative Humidity measured as', humidity, '%')
 
     result = measure_Si7021.measure_both(i2c, addr_Si7021)
-    print("RH measured as", result['humi'], "%, \t temp measured as", result['temp'], "oC")
+    print('RH measured as', result['humi'], '%, \t temp measured as', result['temp'], 'oC')
 
     time.sleep_ms(250)
