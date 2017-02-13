c90d806c1a0518dab556265974cfea1bd4ca0d73
JHertz5 <jukka.hertzog@live.co.uk>
2017-02-13 18:05:37 +0000

    mqtt_sub now reads time from broker and data from device

--- device_src/mqtt_sub.py
+++ device_src/mqtt_sub.py
@@ -2,18 +2,28 @@ import paho.mqtt.client as mqtt
 import json
 
 # runs when successful connection to server is made
-def on_connect(client, userdata, rc):
+def on_connect_data(client, userdata, rc):
     print("Connected with result code " + str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(topic_prefix)
-    print("subscribed to", topic_prefix)
+    topic_data = 'esys/sexual-keiling/'
+    client.subscribe(topic_data)
+    print("subscribed to", topic_data)
+
+# runs when successful connection to server is made
+def on_connect_time(client, userdata, rc):
+    print("Connected with result code " + str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    topic_time = 'esys/time'
+    client.subscribe(topic_time)
+    print("subscribed to", topic_time)
 
 # runs when message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
     msg_string = msg.payload.decode('UTF-8')
-    dict_derulo = json.loads(msg_string)
+    #dict_derulo = json.loads(msg_string)
 
 print('\n******************************************')
 print('Welcome to the Sexual Keiling MQTT monitor')
@@ -22,21 +32,24 @@ print('Are you connected to the EEERover wifi?')
 input('Press Enter to continue\n')
 
 broker_address = '192.168.0.10'
-#broker_address = 'iot.eclipse.org' # test server
-topic_prefix = 'esys/sexual-keiling/'
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+client_data = mqtt.Client()
+client_data.on_connect = on_connect_data
+client_data.on_message = on_message
+
+client_time = mqtt.Client()
+client_time.on_connect = on_connect_time
+client_time.on_message = on_message
 
 # todo check paho doc for a way to save this from infinite loop if no connection is made
-client.connect(broker_address)
+client_data.connect(broker_address)
+client_time.connect(broker_address)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-#client.loop_forever()
+#client_data.loop_forever()
 while 1:
-    client.loop(1.0)
-    
+    client_data.loop(0.1)
+    client_time.loop(0.1)
